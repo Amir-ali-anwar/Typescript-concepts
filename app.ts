064c8ec07acbf5e06ffbb21648f0cc1b9c8ed14a
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-09-04 18:11:28 +0500

    Opp with ts

--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-console.log('Your code goes here amir is the top developer of seebiz...');
+class Department {
+    private employees:string[]=[];
+    constructor(private id: string, public name: String) {
+    }
+    showUser(this:Department){
+        console.log(`${this.id} ${this.name}`)
+    }
+    addUser(user:string){
+    this.employees.push(user)
+    }
+    showUserInfo(){
+        console.log(this.employees)
+    }
+}
+let output = new Department('1', 'name')
+output.showUser();
+output.addUser('amir')
+output.showUserInfo()
