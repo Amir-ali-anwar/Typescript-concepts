850f06f7a0026ff7d9e41a22bffdda567cebd504
Amir Ali anwar <amiralianwar611@gmail.com>
2024-03-09 20:47:43 +0500

    solving the Js challanges

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -9,7 +9,7 @@ function sum(message: string, ...numbers: number[]) {
 
 }
 
-let result = sum(`The total is : `, 1, 2, 3, 4)
+// let result = sum(`The total is : `, 1, 2, 3, 4)
 // console.log(result);
 
 
@@ -27,6 +27,31 @@ const processInput = (input: string | number) => {
 
   }
 }
-processInput(10);
-processInput('Hello');
+// processInput(10);
+// processInput('Hello');
 
+
+
+
+//Your task is to create a function named processData that accepts two parameters:
+
+//The first parameter, input, should be a union type that can be either a string or a number.
+//The second parameter, config, should be an object with a reverse property of type boolean, by default it "reverse" should be false
+//The function should behave as follows:
+
+//If input is of type number, the function should return the square of the number.
+//If input is of type string, the function should return the string in uppercase.
+//If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
+
+const processData = (input: string | number, config: { reverse: boolean } = { reverse: false }): string | Number => {
+  if(typeof input ==='number'){
+    return input * input;
+  }else{
+    return config.reverse
+    ? input.toUpperCase().split('').reverse().join('')
+    : input.toUpperCase();
+  }
+
+}
+console.log(processData(10)); 
+console.log(processData('Amir Ali Anwar')); 
