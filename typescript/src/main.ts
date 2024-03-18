eee51ce3b884b8950c95b5c21beca62ae33f506e
Amir Ali anwar <amiralianwar611@gmail.com>
2024-03-18 22:17:12 +0500

    adding Enums examples

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -268,3 +268,25 @@ type animal = {
   [personAge]: number
 }
 let tiger:animal= {[personAge]:5}; 
+
+// Enums
+
+// Enums in TypeScript allow us to define a set of named constants.
+// Using enums can make it easier to document intent, or create a set of distinct cases.
+enum ServerResponseStatus {
+  Success = 200,
+  Error = 'Error',
+}
+interface ServerResponse {
+  result: ServerResponseStatus;
+  data: string[];
+}
+
+function serverResponse():ServerResponse{
+  return {
+    result:ServerResponseStatus.Success,
+    data:['first Item','second Item']
+  }
+}
+const response:ServerResponse= serverResponse()
+console.log(response);
